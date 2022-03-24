d277022f7c8b3dc132606395a52a3c8504fe0115
Kostiantyn Povnych <kostiantyn.povnych@gmail.com>
2022-03-24 17:16:16 +0200

    criterion

--- example-performance-test/src/main.rs
+++ example-performance-test/src/main.rs
@@ -1,20 +1,3 @@
-#![feature(test)]
-
-extern crate test;
-
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    use test::Bencher;
-
-    #[test]
-    fn test1() {}
-
-    #[bench]
-    fn bench1(b: &mut Bencher) {
-        b.iter(|| std::thread::sleep(std::time::Duration::from_millis(100)));
-    }
 }
