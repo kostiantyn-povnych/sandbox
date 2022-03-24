315ce8a40c22f5d2946aeced9a4c6a9ff75a7788
github-action-benchmark <github@users.noreply.github.com>
2022-03-24 13:38:55 +0000

    add Benchmark (cargo) benchmark result for 4a9209b94ca151acd3f3ffb345ff4ec7360ca538

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1648129135693,
+  "repoUrl": "https://github.com/kostiantyn-povnych/sandbox",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "82378267+kostiantyn-povnych@users.noreply.github.com",
+            "name": "kostiantyn-povnych",
+            "username": "kostiantyn-povnych"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4a9209b94ca151acd3f3ffb345ff4ec7360ca538",
+          "message": "Update rust.yml",
+          "timestamp": "2022-03-24T15:37:55+02:00",
+          "tree_id": "032003171265a234677003f22b1cc60f74aa59d2",
+          "url": "https://github.com/kostiantyn-povnych/sandbox/commit/4a9209b94ca151acd3f3ffb345ff4ec7360ca538"
+        },
+        "date": 1648129134760,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench1",
+            "value": 100094395,
+            "range": "± 13385",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
